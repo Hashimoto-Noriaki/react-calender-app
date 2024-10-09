aff7379e79006fce1d2e17d7bd606b0886ecd9a3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-10-10 04:45:38 +0900

    不要なコメント削除

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </div>
   </React.StrictMode>,
 )
-
-{/* <div className="flex items-center justify-center h-screen">
-<h1 className="text-3xl font-bold underline">React練習</h1> */}
