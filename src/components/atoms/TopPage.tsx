2658caa7f7eb2811027834545bba68aafec7d67b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-10-10 16:00:02 +0900

    logoにstyleを適用

--- src/components/atoms/TopPage.tsx
+++ src/components/atoms/TopPage.tsx
@@ -5,7 +5,7 @@ export const TopPage = () => {
         <div>
             <header className="leading-[50px]">
                 <div className="container mx-auto flex justify-between">
-                    <p>スケジュール管理</p>
+                    <p className="logo">スケジュール管理</p>
                     <nav>
                         <ul className="flex gap-5">
                             <li>利用方法</li>
