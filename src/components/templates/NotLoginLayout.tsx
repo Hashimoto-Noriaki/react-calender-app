e2c35a67d99b7295e7ea6cd08f6d95e642907885
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-10-12 13:06:31 +0900

    NotLoginのコードをLoginPageから移動

---
+++ src/components/templates/NotLoginLayout.tsx
@@ -0,0 +1,17 @@
+export const NotLoginLayout = () => {
+  return (
+    <div className="relative">
+        <header className="bg-white leading-[50px] fixed top-0  left-0 right-0">
+            <div className="container mx-auto flex justify-between">
+                <p className="logo">スケジュール管理</p>
+                <nav>
+                    <ul className="flex gap-5 text-emerald-800">
+                        <li>利用方法</li>
+                        <li>ログイン</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </div>
+  )
+}
