50757df8163eba63f8430bd65f12279e69ca5c28
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-10-13 23:11:17 +0900

    ログイン画面の枠をエメラルド

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -4,7 +4,7 @@ export const Input = (props: ComponentProps<"input">) => {
   return (
     <input
         {...props}
-        className="w-full border-emerald-800 border-solid rounded-md p-2"
+        className="w-full border-4 border-emerald-800 border-solid rounded-md p-2"
     />
   )
 }
