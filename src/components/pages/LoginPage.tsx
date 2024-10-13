771dc4f5113019ea4d618a32f9ce22eecaf355df
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-10-13 19:42:11 +0900

    inputタグにてstateが変更できるように、name、value、onChangeをセット

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { ChangeEvent,useState } from "react"
 import { Input } from "../atoms/Input"
 import { PrimaryBtn } from "../atoms/PrimaryBtn"
 import { LoginInfoType } from "../../types/login"
@@ -7,8 +7,13 @@ export const LoginPage = () => {
     const [loginInfo, setLoginInfo] = useState<LoginInfoType>({
         email: "",
         password: "",
-      })
-    
+    })
+
+    const changeLoginInfo = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target
+        setLoginInfo({ ...loginInfo, [name]: value })
+    }
+
     return (
     <div className="w-[500px] bg-white rounded-lg shadow-lg py-10">
         <form className="flex flex-col justify-center items-center gap-10">
@@ -17,14 +22,20 @@ export const LoginPage = () => {
             </h1>
             <div className="w-[80%]">
                 <Input
+                    name = "email"
                     type = "text"
                     placeholder = "email"
+                    value = {loginInfo.email}
+                    onChange={changeLoginInfo}
                 />
             </div>
             <div className="w-[80%]">
                 <Input
+                    name = "password"
                     type = "password"
                     placeholder = "password"
+                    value = {loginInfo.password}
+                    onChange={changeLoginInfo}
                 />
             </div>
             <PrimaryBtn onClick={()=> null}>ログイン</PrimaryBtn>
