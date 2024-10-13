f9d5e17e3902745aef9679287cfa1633427d210b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-10-13 23:11:00 +0900

    ログイン画面のエラーメッセージを追加

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,7 +1,8 @@
-import { ChangeEvent,useState } from "react"
+import { ChangeEvent,FormEvent,useState } from "react"
 import { Input } from "../atoms/Input"
 import { PrimaryBtn } from "../atoms/PrimaryBtn"
 import { LoginInfoType } from "../../types/login"
+import { login } from "../../api/login"
 
 export const LoginPage = () => {
     const [loginInfo, setLoginInfo] = useState<LoginInfoType>({
@@ -9,17 +10,34 @@ export const LoginPage = () => {
         password: "",
     })
 
+    const [errorMessage,setErrorMessage] = useState("");
+
     const changeLoginInfo = (event: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target
         setLoginInfo({ ...loginInfo, [name]: value })
     }
 
+    const handleLogin = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        setErrorMessage("")
+        try {
+          login(loginInfo)
+        } catch {
+          setErrorMessage("ログインに失敗しました");
+        }
+    };
+
     return (
     <div className="w-[500px] bg-white rounded-lg shadow-lg py-10">
-        <form className="flex flex-col justify-center items-center gap-10">
-            <h1 className="text-3xl text-emerald-800 font-bold text-ceneter" >
+        <form className="flex flex-col justify-center items-center gap-10" onSubmit={handleLogin}>
+            <h1 className="text-3xl text-emerald-800 font-bold text-center" >
                 ログイン
             </h1>
+            {errorMessage !== "" &&(
+                <div className="p-5 bg-red-500 text-white rounded-lg w-[80%]">
+                {errorMessage}
+                </div>
+            )}
             <div className="w-[80%]">
                 <Input
                     name = "email"
